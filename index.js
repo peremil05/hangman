7358e1c3525e67fb9a17cd3350df9547679a5062
Emil Jansson <emiljansson05@outlook.com>
2022-12-25 11:57:44 +0100

    Added random word selection

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-var word = "osteoporosis";
+
+var word = "default";
+var listOfWords = "./words.txt";
 
 var textHolder = document.getElementById("text");
 
-var completedArray = new Array(word.length).fill('_');
-var leftArray = word.toLowerCase().split('');
+var completedArray;
+var leftArray; 
+
+let getRandomWord = (wordList) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', wordList, false);
+
+    xhr.onload = () => {
+        let array = xhr.response.split("\r\n");
+        word = array[Math.floor(Math.random()*array.length)];
+    };
+
+    xhr.send();
+
+    return word;
+}
+
+let prepareArrays = (word) => {
+    completedArray = new Array(word.length).fill('_');
+    leftArray = word.toLowerCase().split('');
+}
+
+word = getRandomWord(listOfWords);
+
+prepareArrays(word);
 
 textHolder.innerHTML = completedArray.join(' ');
 
